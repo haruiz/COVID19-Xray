d0f6303502d44d3b08678cecd0f4b92aa91c37d1
Henry Ruiz <henry.ruiz@tamu.edu>
2020-09-12 09:57:33 -0500

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -9,7 +9,7 @@ def covid19(pretrained=True, **kwargs):
     model=create_model()
     if pretrained:
         #checkpoint_uri="https://github.com/haruiz/COVID19-Xray/blob/master/covid19-1e76d47b.pt?raw=true"
-        checkpoint_uri = "https://modelshub.blob.core.windows.net/models/covid19-1e76d47b.pt"
+        checkpoint_uri = "https://haruiz-models.s3.amazonaws.com/covid19-1e76d47b.pt"
         checkpoint=torch.hub.load_state_dict_from_url(checkpoint_uri,progress=False,map_location='cpu')
         model.load_state_dict(checkpoint)
     return model
